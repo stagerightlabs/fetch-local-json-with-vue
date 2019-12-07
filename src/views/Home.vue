d9b41ce5394f5e29af0ec8006fb3f8f082999e6c
Ryan C. Durham <ryan@stagerightlabs.com>
2019-12-07 15:14:21 -0800

    Read local json file and display contents

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="loaded">
+      <h2>{{ dataSetName }}</h2>
+      <h3>https://catalog.data.gov/dataset/pm2-5-air-quality-standard-update</h3>
+      <div v-for="complaint in filteredData" :key="complaint[1]">
+        <p style="margin-bottom: 20px; text-align:justify">
+          {{ complaint[13] }}
+          <br />
+          - {{ complaint[8] }} {{ complaint[9] }}
+        </p>
+      </div>
+    </div>
+    <p v-else>Loading...</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      airData: {},
+      loaded: false
+    }
+  },
+  computed: {
+    filteredData () {
+      return this.airData.data.filter(d => d[13])
+    },
+    dataSetName () {
+      return this.airData.meta.view.name
+    }
+  },
+  methods: {
+    getJsonData () {
+      fetch('air_quality.json')
+        .then(response => response.json())
+        .then(data => {
+          this.airData = data
+          this.loaded = true
+        })
+    }
+  },
+  mounted () {
+    this.getJsonData()
   }
 }
 </script>
