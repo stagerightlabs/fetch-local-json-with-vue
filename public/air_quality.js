09d59feda7c43005ba20717e07678b2e4408007a
Ryan C. Durham <ryan@stagerightlabs.com>
2019-12-08 14:28:51 -0800

    Import JSON via global var

---
+++ public/air_quality.js
@@ -0,0 +1,358 @@
+var AIRQUALITY = {
+  'meta': {
+    'view': {
+      'id': 'chv8-dyu5',
+      'name': 'PM2.5 Air Quality Standard Update',
+      'averageRating': 0,
+      'createdAt': 1434145664,
+      'displayType': 'table',
+      'downloadCount': 1481,
+      'hideFromCatalog': false,
+      'hideFromDataJson': false,
+      'indexUpdatedAt': 1440111464,
+      'newBackend': true,
+      'numberOfComments': 0,
+      'oid': 11780389,
+      'provenance': 'official',
+      'publicationAppendEnabled': true,
+      'publicationDate': 1434386368,
+      'publicationGroup': 3158678,
+      'publicationStage': 'published',
+      'rowsUpdatedAt': 1440111379,
+      'tableId': 3188808,
+      'totalTimesRated': 0,
+      'viewCount': 459,
+      'viewLastModified': 1434386368,
+      'viewType': 'tabular',
+      'approvals': [ {
+        'reviewedAt': 1434386368,
+        'reviewedAutomatically': true,
+        'state': 'approved',
+        'submissionId': 496553,
+        'submissionObject': 'public_audience_request',
+        'submissionOutcome': 'change_audience',
+        'submittedAt': 1434386368,
+        'workflowId': 1508,
+        'submissionDetails': {
+          'permissionType': 'READ'
+        },
+        'submissionOutcomeApplication': {
+          'failureCount': 0,
+          'status': 'success'
+        },
+        'submitter': {
+          'id': '44u9-wper',
+          'displayName': 'MT Oregon DEQ'
+        }
+      } ],
+      'columns': [ {
+        'id': -1,
+        'name': 'sid',
+        'dataTypeName': 'meta_data',
+        'fieldName': ':sid',
+        'position': 0,
+        'renderTypeName': 'meta_data',
+        'format': { },
+        'flags': [ 'hidden' ]
+      }, {
+        'id': -1,
+        'name': 'id',
+        'dataTypeName': 'meta_data',
+        'fieldName': ':id',
+        'position': 0,
+        'renderTypeName': 'meta_data',
+        'format': { },
+        'flags': [ 'hidden' ]
+      }, {
+        'id': -1,
+        'name': 'position',
+        'dataTypeName': 'meta_data',
+        'fieldName': ':position',
+        'position': 0,
+        'renderTypeName': 'meta_data',
+        'format': { },
+        'flags': [ 'hidden' ]
+      }, {
+        'id': -1,
+        'name': 'created_at',
+        'dataTypeName': 'meta_data',
+        'fieldName': ':created_at',
+        'position': 0,
+        'renderTypeName': 'meta_data',
+        'format': { },
+        'flags': [ 'hidden' ]
+      }, {
+        'id': -1,
+        'name': 'created_meta',
+        'dataTypeName': 'meta_data',
+        'fieldName': ':created_meta',
+        'position': 0,
+        'renderTypeName': 'meta_data',
+        'format': { },
+        'flags': [ 'hidden' ]
+      }, {
+        'id': -1,
+        'name': 'updated_at',
+        'dataTypeName': 'meta_data',
+        'fieldName': ':updated_at',
+        'position': 0,
+        'renderTypeName': 'meta_data',
+        'format': { },
+        'flags': [ 'hidden' ]
+      }, {
+        'id': -1,
+        'name': 'updated_meta',
+        'dataTypeName': 'meta_data',
+        'fieldName': ':updated_meta',
+        'position': 0,
+        'renderTypeName': 'meta_data',
+        'format': { },
+        'flags': [ 'hidden' ]
+      }, {
+        'id': -1,
+        'name': 'meta',
+        'dataTypeName': 'meta_data',
+        'fieldName': ':meta',
+        'position': 0,
+        'renderTypeName': 'meta_data',
+        'format': { },
+        'flags': [ 'hidden' ]
+      }, {
+        'id': 208700336,
+        'name': 'First Name',
+        'dataTypeName': 'text',
+        'fieldName': 'first_name',
+        'position': 1,
+        'renderTypeName': 'text',
+        'tableColumnId': 28991783,
+        'width': 100,
+        'cachedContents': {
+          'non_null': 5,
+          'largest': 'Paul',
+          'null': '0',
+          'top': [ {
+            'item': 'John',
+            'count': '2'
+          }, {
+            'item': 'Paul',
+            'count': '1'
+          }, {
+            'item': 'Kathryn ',
+            'count': '1'
+          }, {
+            'item': 'Michael D',
+            'count': '1'
+          } ],
+          'smallest': 'John',
+          'not_null': '5',
+          'cardinality': '4'
+        },
+        'format': { }
+      }, {
+        'id': 208700337,
+        'name': 'Last Name',
+        'dataTypeName': 'text',
+        'fieldName': 'last_name',
+        'position': 2,
+        'renderTypeName': 'text',
+        'tableColumnId': 28991784,
+        'width': 100,
+        'cachedContents': {
+          'non_null': 5,
+          'largest': 'Woods',
+          'null': '0',
+          'top': [ {
+            'item': 'Krallman',
+            'count': '2'
+          }, {
+            'item': 'Ruscher',
+            'count': '1'
+          }, {
+            'item': 'VanNatta',
+            'count': '1'
+          }, {
+            'item': 'Woods',
+            'count': '1'
+          } ],
+          'smallest': 'Krallman',
+          'not_null': '5',
+          'cardinality': '4'
+        },
+        'format': { }
+      }, {
+        'id': 208700338,
+        'name': 'Email Address',
+        'dataTypeName': 'text',
+        'fieldName': 'email_address',
+        'position': 3,
+        'renderTypeName': 'text',
+        'tableColumnId': 28991785,
+        'width': 100,
+        'cachedContents': {
+          'non_null': 5,
+          'largest': 'pruscher@gmail.com',
+          'null': '0',
+          'top': [ {
+            'item': 'kathryn@nwpulpandpaper.org',
+            'count': '1'
+          }, {
+            'item': 'john@whatinsourair.org',
+            'count': '1'
+          }, {
+            'item': 'pruscher@gmail.com',
+            'count': '1'
+          }, {
+            'item': 'mwoodspdx@centurylink.net',
+            'count': '1'
+          }, {
+            'item': 'john@whatsinourair.org',
+            'count': '1'
+          } ],
+          'smallest': 'john@whatinsourair.org',
+          'not_null': '5',
+          'cardinality': '5'
+        },
+        'format': {
+          'displayStyle': 'email'
+        }
+      }, {
+        'id': 208700339,
+        'name': 'Organization',
+        'dataTypeName': 'text',
+        'fieldName': 'organization',
+        'position': 4,
+        'renderTypeName': 'text',
+        'tableColumnId': 28991786,
+        'width': 100,
+        'cachedContents': {
+          'non_null': 3,
+          'largest': 'NWPPA',
+          'null': '2',
+          'top': [ {
+            'item': 'Neighbors for Clean Air',
+            'count': '2'
+          }, {
+            'item': 'NWPPA',
+            'count': '1'
+          } ],
+          'smallest': 'Neighbors for Clean Air',
+          'not_null': '3',
+          'cardinality': '2'
+        },
+        'format': { }
+      }, {
+        'id': 208700340,
+        'name': 'State',
+        'dataTypeName': 'text',
+        'fieldName': 'state',
+        'position': 5,
+        'renderTypeName': 'text',
+        'tableColumnId': 28991787,
+        'width': 100,
+        'cachedContents': {
+          'non_null': 5,
+          'largest': 'Oregon 97266',
+          'null': '0',
+          'top': [ {
+            'item': 'Oregon',
+            'count': '3'
+          }, {
+            'item': 'Oregon 97266',
+            'count': '1'
+          }, {
+            'item': 'OR',
+            'count': '1'
+          } ],
+          'smallest': 'OR',
+          'not_null': '5',
+          'cardinality': '3'
+        },
+        'format': { }
+      }, {
+        'id': 208700341,
+        'name': 'Comment',
+        'dataTypeName': 'text',
+        'fieldName': 'comment',
+        'position': 6,
+        'renderTypeName': 'text',
+        'tableColumnId': 28991793,
+        'width': 100,
+        'cachedContents': {
+          'non_null': 4,
+          'largest': "What about a time limit for wood burning stoves/furnaces in the city areas? I'm in SE Portland, the temperature right now is 75 degrees and my neighbor is burning woods for no reason I can find, this has been going on since the weather has warmed up, even on the 80 to 90 degrees days we've had. All winter he never burned, just once it warms up. Today marks the third day he's been burning 24 hours a day. \n\nWhy not a ban on wood burning from something like the same time studded tires have to be removed?\n\nThat's plenty of time to save money on fuel costs and give the rest of us time to enjoy the spring and summer months.\n\nThank you for your time.... Michael",
+          'null': '1',
+          'top': [ {
+            'item': 'See attached comments.',
+            'count': '1'
+          }, {
+            'item': "I am writing as a private individual but am sharing my professional affiliations as well, just for illustrative purposes.  I am strongly in favor of the department's proposal to adopt the federal PM 2.5 standards, particularly if they are modified in the future to afford increased public protection.\n\nI am an asthma sufferer and am susceptible to small particle contamination and triggers and urge the state to impose this guideline on any industry or practice that has the potential to do harm.  I'm also a rational person who understands the need for forest protection and best management practices and I believe that there are ways to ensure that controlled burns can be done in a way that also protects air quality for the vast majority of Oregonians even in areas like Oakridge which have been in violation in the past.  \n\nBy taking into account proper understanding of windflow in complex terrain, there are ways in which you can minimize risks to the populace and still reduce long-term risks of catastrophic wildfires which are also a real hazard.  \n\nI urge DEQ and the Division of Forestry and the BLM and USFS to work together to come up with meaningful ways in which controlled burns on rangelands, grasslands, and forest lands are still allowed when meteorological conditions warrant for protection of natural and managed areas, while still protecting air quality for those of us in \"sensitive populations,\" as well.\n\nThank you for considering my request.\n\nSincerely,\n\nPaul Ruscher, PhD\nFellow, American Meteorological Society\nCoordinator, Watershed Science Technician Program\nFaculty Lead, Earth & Environmental Sciences, Lane Community College",
+            'count': '1'
+          }, {
+            'item': "What about a time limit for wood burning stoves/furnaces in the city areas? I'm in SE Portland, the temperature right now is 75 degrees and my neighbor is burning woods for no reason I can find, this has been going on since the weather has warmed up, even on the 80 to 90 degrees days we've had. All winter he never burned, just once it warms up. Today marks the third day he's been burning 24 hours a day. \n\nWhy not a ban on wood burning from something like the same time studded tires have to be removed?\n\nThat's plenty of time to save money on fuel costs and give the rest of us time to enjoy the spring and summer months.\n\nThank you for your time.... Michael",
+            'count': '1'
+          }, {
+            'item': 'Please see attached comments.',
+            'count': '1'
+          } ],
+          'smallest': "I am writing as a private individual but am sharing my professional affiliations as well, just for illustrative purposes.  I am strongly in favor of the department's proposal to adopt the federal PM 2.5 standards, particularly if they are modified in the future to afford increased public protection.\n\nI am an asthma sufferer and am susceptible to small particle contamination and triggers and urge the state to impose this guideline on any industry or practice that has the potential to do harm.  I'm also a rational person who understands the need for forest protection and best management practices and I believe that there are ways to ensure that controlled burns can be done in a way that also protects air quality for the vast majority of Oregonians even in areas like Oakridge which have been in violation in the past.  \n\nBy taking into account proper understanding of windflow in complex terrain, there are ways in which you can minimize risks to the populace and still reduce long-term risks of catastrophic wildfires which are also a real hazard.  \n\nI urge DEQ and the Division of Forestry and the BLM and USFS to work together to come up with meaningful ways in which controlled burns on rangelands, grasslands, and forest lands are still allowed when meteorological conditions warrant for protection of natural and managed areas, while still protecting air quality for those of us in \"sensitive populations,\" as well.\n\nThank you for considering my request.\n\nSincerely,\n\nPaul Ruscher, PhD\nFellow, American Meteorological Society\nCoordinator, Watershed Science Technician Program\nFaculty Lead, Earth & Environmental Sciences, Lane Community College",
+          'not_null': '4',
+          'cardinality': '4'
+        },
+        'format': { }
+      }, {
+        'id': 208700342,
+        'name': 'Additional Document',
+        'dataTypeName': 'document',
+        'fieldName': 'additional_document',
+        'position': 7,
+        'renderTypeName': 'document',
+        'tableColumnId': 28991813,
+        'width': 100,
+        'cachedContents': {
+          'non_null': 5,
+          'null': 0
+        },
+        'format': { },
+        'subColumnTypes': [ 'content_type', 'file_id', 'filename', 'size' ]
+      } ],
+      'grants': [ {
+        'inherited': false,
+        'type': 'viewer',
+        'flags': [ 'public' ]
+      } ],
+      'metadata': {
+        'availableDisplayTypes': [ 'table', 'fatrow', 'page' ],
+        'renderTypeConfig': {
+          'visible': {
+            'table': true
+          }
+        }
+      },
+      'owner': {
+        'id': '44u9-wper',
+        'displayName': 'MT Oregon DEQ',
+        'screenName': 'MT Oregon DEQ',
+        'type': 'interactive',
+        'userSegment': 'site_member',
+        'flags': [ 'acceptedEula' ]
+      },
+      'query': { },
+      'rights': [ 'read' ],
+      'tableAuthor': {
+        'id': '44u9-wper',
+        'displayName': 'MT Oregon DEQ',
+        'screenName': 'MT Oregon DEQ',
+        'type': 'interactive',
+        'userSegment': 'site_member',
+        'flags': [ 'acceptedEula' ]
+      },
+      'flags': [ 'default', 'restorable', 'restorePossibleForType' ]
+    }
+  },
+  'data': [ [ 'row-u7ri.6hqs.kzhb', '00000000-0000-0000-BDAF-AF2D23F25A44', 0, 1568303959, null, 1568303959, null, '{ }', 'Paul', 'Ruscher', 'pruscher@gmail.com', null, 'Oregon', "I am writing as a private individual but am sharing my professional affiliations as well, just for illustrative purposes.  I am strongly in favor of the department's proposal to adopt the federal PM 2.5 standards, particularly if they are modified in the future to afford increased public protection.\n\nI am an asthma sufferer and am susceptible to small particle contamination and triggers and urge the state to impose this guideline on any industry or practice that has the potential to do harm.  I'm also a rational person who understands the need for forest protection and best management practices and I believe that there are ways to ensure that controlled burns can be done in a way that also protects air quality for the vast majority of Oregonians even in areas like Oakridge which have been in violation in the past.  \n\nBy taking into account proper understanding of windflow in complex terrain, there are ways in which you can minimize risks to the populace and still reduce long-term risks of catastrophic wildfires which are also a real hazard.  \n\nI urge DEQ and the Division of Forestry and the BLM and USFS to work together to come up with meaningful ways in which controlled burns on rangelands, grasslands, and forest lands are still allowed when meteorological conditions warrant for protection of natural and managed areas, while still protecting air quality for those of us in \"sensitive populations,\" as well.\n\nThank you for considering my request.\n\nSincerely,\n\nPaul Ruscher, PhD\nFellow, American Meteorological Society\nCoordinator, Watershed Science Technician Program\nFaculty Lead, Earth & Environmental Sciences, Lane Community College", null ],
+    [ 'row-py2k.4uyx~3ifk', '00000000-0000-0000-864C-50A458550FDD', 0, 1568303959, null, 1568303959, null, '{ }', 'Michael D', 'Woods', 'mwoodspdx@centurylink.net', null, 'Oregon 97266', "What about a time limit for wood burning stoves/furnaces in the city areas? I'm in SE Portland, the temperature right now is 75 degrees and my neighbor is burning woods for no reason I can find, this has been going on since the weather has warmed up, even on the 80 to 90 degrees days we've had. All winter he never burned, just once it warms up. Today marks the third day he's been burning 24 hours a day. \n\nWhy not a ban on wood burning from something like the same time studded tires have to be removed?\n\nThat's plenty of time to save money on fuel costs and give the rest of us time to enjoy the spring and summer months.\n\nThank you for your time.... Michael", null ],
+    [ 'row-u97r-bqfd_m5rw', '00000000-0000-0000-8117-E678D7CC9985', 0, 1568303959, null, 1568303959, null, '{ }', 'Kathryn ', 'VanNatta', 'kathryn@nwpulpandpaper.org', 'NWPPA', 'OR', 'Please see attached comments.', [ null, 'si4rRKHJ3cjyc_XWVMJsq3FT5-fDgXY4RdNCX0za_r4', 'NWPPA Comments DEQ PM SIP and Interstate Transport Elements Aug 19 2015 .docx', null ] ],
+    [ 'row-nvwb-gd4w_qv6h', '00000000-0000-0000-0236-9DF4123427A4', 0, 1568303959, null, 1568303959, null, '{ }', 'John', 'Krallman', 'john@whatinsourair.org', 'Neighbors for Clean Air', 'Oregon', null, null ],
+    [ 'row-8qx9-6saq~7y5i', '00000000-0000-0000-471F-6FE8CDFF3DE1', 0, 1568303959, null, 1568303959, null, '{ }', 'John', 'Krallman', 'john@whatsinourair.org', 'Neighbors for Clean Air', 'Oregon', 'See attached comments.', [ null, 'VDtzefR4ik3PJOXkDTvl_dejOUFeubaksm5ZJ5agiWI', 'PM Infrastructure SIP Comment.docx', null ] ]
+  ]
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="home">
-    <div v-if="loaded">
-      <h2>{{ dataSetName }}</h2>
-      <h3>https://catalog.data.gov/dataset/pm2-5-air-quality-standard-update</h3>
-      <div v-for="complaint in filteredData" :key="complaint[1]">
-        <p style="margin-bottom: 20px; text-align:justify">
-          {{ complaint[13] }}
-          <br />
-          - {{ complaint[8] }} {{ complaint[9] }}
-        </p>
-      </div>
+    <h2>{{ dataSetName }}</h2>
+    <h3>https://catalog.data.gov/dataset/pm2-5-air-quality-standard-update</h3>
+    <div v-for="complaint in filteredData" :key="complaint[1]">
+      <p style="margin-bottom: 20px; text-align:justify">
+        {{ complaint[13] }}
+        <br />
+        - {{ complaint[8] }} {{ complaint[9] }}
+      </p>
     </div>
-    <p v-else>Loading...</p>
   </div>
 </template>
 
@@ -21,7 +18,9 @@ export default {
   name: 'home',
   data () {
     return {
-      airData: {},
+      /*eslint-disable */
+      airData: AIRQUALITY,
+      /* eslint-enable */
       loaded: false
     }
   },
@@ -32,19 +31,6 @@ export default {
     dataSetName () {
       return this.airData.meta.view.name
     }
-  },
-  methods: {
-    getJsonData () {
-      fetch('air_quality.json')
-        .then(response => response.json())
-        .then(data => {
-          this.airData = data
-          this.loaded = true
-        })
-    }
-  },
-  mounted () {
-    this.getJsonData()
   }
 }
 </script>
